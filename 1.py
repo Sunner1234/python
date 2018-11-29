a316443a50ab9d5563ed749f59c6d34ee88532c9
Sunner1234 <45253821+Sunner1234@users.noreply.github.com>
2018-11-29 16:58:36 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+#展示文件位置代码
 import os
 root_path = os.getcwd()
 offset = len(root_path.split("\\"))
@@ -10,6 +11,19 @@ for root,dirs,files in os.walk(root_path):
 	for f in files:
 		filename = os.path.splitext(f)[0]   #输出文件名取第0个文件赋予filename名字 
 		print("\t"*(indent_level+1),filename)
+
+#读行函数代码
+def line_count(file_path):
+	code_line,blank_line = 0,0      #定义两个变量
+	with open(file_path,'r') as fp: 
+		while True:                 
+			line = fp.readline()    #读line
+			if not line:
+				break
+			code_line+=1
+	print(code_line,"lines")
+line_count(file_path)
+
 
 
 
